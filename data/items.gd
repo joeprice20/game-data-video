6a9463e8664542902ebcbdecbcb41b00e9cb53bd
Joe <joeaidprice@gmail.com>
2026-02-22 11:49:23 +0000

    add sword item

--- data/items.gd
+++ data/items.gd
@@ -2,6 +2,11 @@ class_name Items
 
 const Database = {
 	"pickaxe" : {
-		"name" : "Pickaxe"
+		"name" : "Pickaxe",
+		"scene" : "res://elements/pickaxe/pickaxe.glb"
+	},
+	"sword" : {
+		"name" : "Sword",
+		"scene" : "res://elements/sword/sword.glb"
 	}
 }
